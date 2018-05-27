72c26be2ad96f656d5c782a7a8a5d9cc5c4a7f24
Mayank <rajoria_m@yahoo.com>
2018-05-27 17:44:06 +0530

    Initial commit

---
+++ app/src/main/java/com/mayankrajoria/logintimeout/LoginActivity.java
@@ -0,0 +1,118 @@
+package com.mayankrajoria.logintimeout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button loginButton;
+    EditText useridET, passET;
+    final String USER_ID = "user";
+    final String PASSWORD = "pass";
+    final String MyPREFERENCES = "com.mayankrajoria.loginstate";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String loggedinState = sharedpreferences.getString("loggedin", "no");
+        Log.d("LA", "Checking state on create: " + loggedinState);
+        if(loggedinState.equals("yes")) {
+            if(checkSession()) {
+                Intent it = new Intent(this, UserActivity.class);
+                startActivity(it);
+                finish();
+            }
+        }
+
+        loginButton = (Button) findViewById(R.id.login_button);
+        useridET = (EditText) findViewById(R.id.userid_et);
+        passET = (EditText) findViewById(R.id.pass_et);
+        loginButton.setOnClickListener(this);
+    }
+
+    boolean checkSession() {
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String loggedinState = sharedpreferences.getString("loggedin", "no");
+        if(loggedinState.equals("yes")) {
+            long startTime = sharedpreferences.getLong("starttime", 0);
+            long millis = System.currentTimeMillis() - startTime;
+            if(millis/1000 > 20) {
+                Toast.makeText(this, "Your previous session has expired", Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("loggedin", "no");
+                editor.putLong("starttime", 0);
+                editor.commit();
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String loggedinState = sharedpreferences.getString("loggedin", "no");
+        Log.d("LA", "Checking state on resume: " + loggedinState);
+        if(loggedinState.equals("yes")) {
+            if(checkSession()) {
+                Intent it = new Intent(this, UserActivity.class);
+                startActivity(it);
+                finish();
+            }
+        }
+    }
+
+    boolean validateInput() {
+        String useridString = useridET.getText().toString();
+        if(useridString.equals("")) {
+            Toast.makeText(this, "Enter a Userid", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        String passwordString = passET.getText().toString();
+        if(passwordString.equals("")) {
+            Toast.makeText(this, "Enter a password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!useridString.equals(USER_ID)) {
+            Toast.makeText(this, "Invalid Userid", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!passwordString.equals(PASSWORD)) {
+            Toast.makeText(this, "Invalid Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_button:
+                if(validateInput()) {
+                    SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putString("loggedin", "yes");
+                    editor.commit();
+                    Intent it = new Intent(this, UserActivity.class);
+                    startActivity(it);
+                    finish();
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/mayankrajoria/logintimeout/UserActivity.java
@@ -0,0 +1,93 @@
+package com.mayankrajoria.logintimeout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class UserActivity extends AppCompatActivity {
+    TextView timerTextView;
+    long startTime = 0;
+    final String MyPREFERENCES = "com.mayankrajoria.loginstate";
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Log.d("UA", "startime in runnable: " + startTime);
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            if(seconds > 20) {
+                Log.d("UA", "Logging out on timer end");
+                SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("loggedin", "no");
+                editor.putLong("starttime", 0);
+                editor.commit();
+
+                timerHandler.removeCallbacks(timerRunnable);
+                Intent it = new Intent(getApplicationContext(), LoginActivity.class);
+                Toast.makeText(getApplicationContext(), "Session timed out", Toast.LENGTH_SHORT).show();
+                startActivity(it);
+                finish();
+                return;
+            }
+
+            Log.d("UA", "Setting time in runnable");
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+//            timerTextView.setText("" + millis);
+
+            timerHandler.postDelayed(this, 1000);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        timerTextView = (TextView) findViewById(R.id.timer_TV);
+
+
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        if(sharedpreferences.getLong("starttime", 0) != 0) {
+            startTime = sharedpreferences.getLong("starttime", 0);
+            return;
+        }
+        startTime = System.currentTimeMillis();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        Log.d("UA", "Setting start time in SP");
+        editor.putLong("starttime", startTime);
+        editor.commit();
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        long st = sharedpreferences.getLong("starttime", 0);
+        Log.d("UA", "Checking state on resume in user activity: " + st);
+        if(st != 0) {
+            timerHandler.removeCallbacks(timerRunnable);
+            timerHandler.postDelayed(timerRunnable, 0);
+            startTime = sharedpreferences.getLong("starttime", 0);
+            return;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+}
